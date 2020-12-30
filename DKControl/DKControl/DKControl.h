8a087285810ec1f0d1218d20667f9d00834476a8
sagar trivedi <trivedi.sagar24@gmail.com>
2020-12-30 17:39:45 +0530

    First Commit

---
+++ DKControl/DKControl/DKControl.h
@@ -0,0 +1,18 @@
+//
+//  DKControl.h
+//  DKControl
+//
+//  Created by Trivedi Sagar on 12/30/20.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DKControl.
+FOUNDATION_EXPORT double DKControlVersionNumber;
+
+//! Project version string for DKControl.
+FOUNDATION_EXPORT const unsigned char DKControlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DKControl/PublicHeader.h>
+
+
---
+++ DKControl/DKControl/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  DKControl
+//
+//  Created by Trivedi Sagar on 12/30/20.
+//
+
+import Foundation
+
+public class Service {
+    
+    private init(){
+        
+    }
+    
+    public static func DoSomething() -> String{
+        return "My String Return"
+    }
+}
